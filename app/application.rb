2d9e002b3975a5851d09232f96e918d68420857b
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 15:22:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.current.hour
-    if
+    if time <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
